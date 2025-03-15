7476fd6b69843b54c9f45488d3b7796024cfdc55
Danielle Pashayan <daniellepashayan@gmail.com>
2025-03-15 09:38:04 -0400

    port

--- main.py
+++ main.py
@@ -58,4 +58,4 @@ async def import_healht_data(data: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8100, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
