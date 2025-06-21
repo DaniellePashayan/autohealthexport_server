78557b3270f292ff65dffad1f966890178efa4b1
Danielle Pashayan <daniellepashayan@gmail.com>
2025-06-21 13:22:07 -0400

    made navbar items dynamic

--- dashboard/components/navigation-menu.tsx
+++ dashboard/components/navigation-menu.tsx
@@ -7,6 +7,19 @@ import {
 } from "@/components/ui/navigation-menu";
 import ModeToggle from "./theme-toggle";
 import Link from "next/link";
+import { Settings } from "lucide-react";
+
+interface MenuItem {
+  title: string;
+  url: string;
+}
+
+const MenuItems: MenuItem[] = [
+  { title: "Overview", url: "/overview" },
+  { title: "Body Composition", url: "/body-composition" },
+  { title: "Activity", url: "/activity" },
+  { title: "Nutrition", url: "/nutrition" },
+];
 
 const NavbarMenu = () => {
   return (
@@ -16,30 +29,25 @@ const NavbarMenu = () => {
           {/* Centered menu items */}
           <NavigationMenu className="flex-1 flex justify-center">
             <NavigationMenuList className="flex space-x-4">
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link href="/overview">Overview</Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link href="/body-composition">Body Composition</Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link href="/activity">Activity</Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link href="/nutrition">Nutrition</Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
+              {MenuItems.map((item) => (
+                <NavigationMenuItem key={item.title}>
+                  <NavigationMenuLink asChild>
+                    <Link
+                      href={item.url}
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      {item.title}
+                    </Link>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              ))}
             </NavigationMenuList>
           </NavigationMenu>
           {/* Right-aligned theme toggle */}
           <div className="flex items-center justify-end flex-none ml-4">
+            <Link href="/settings" className={navigationMenuTriggerStyle()}>
+              <Settings className="h-5 w-5" />
+            </Link>
             <ModeToggle />
           </div>
         </nav>
